fad463639271883ff53e3df5cc125f3cbdfa2273
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-07-07 00:52:34 +0000

    Snapshot of branch master taken on 2019-07-06T20:52:33.119-04:00 from "https://github.com/UnofficialJuliaMirror/CBinding.jl-d43a6710-96b8-4a2d-833c-c424785e5374"

---
+++ src/CBinding.jl
@@ -0,0 +1,23 @@
+module CBinding
+	import Libdl
+	using CEnum: @cenum
+	using Todo: @todo_str
+	
+	
+	export Clongdouble, Caggregate, Cstruct, Cunion, Carray, Caccessor, Clibrary, Cglobal, Cglobalconst, Cfunction
+	export @ctypedef, @cstruct, @cunion, @carray, @calign, @cenum
+	
+	
+	# provide a temporary placeholder for 128-bit floating point primitive
+	mutable struct Clongdouble
+		mem::NTuple{16, UInt8}
+		
+		Clongdouble() = new()
+	end
+	
+	
+	include("caggregate.jl")
+	include("clibrary.jl")
+	include("cglobal.jl")
+	include("cfunction.jl")
+end
